6502ab1d12ec7fef4aaef2dab9875ee6daea2d66
Zireael07 <zi@mail.com>
2020-10-19 15:16:40 +0200

    new: basic Rust universe/game state

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,80 @@ macro_rules! log {
     }
 }
 
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Player = 0,
+    Floor = 1,
+    Wall = 2,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+
+// We're storing all the tiles in one big array, so we need a way to map an X,Y coordinate to
+// a tile. Each row is stored sequentially (so 0..20, 21..40, etc.). This takes an x/y and returns
+// the array index.
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 20) + x as usize
+}
+
+// It's a great idea to have a reverse mapping for these coordinates. This is as simple as
+// index % 20 (mod 20), and index / 20
+pub fn idx_xy(idx: usize) -> (i32, i32) {
+    (idx as i32 % 20, idx as i32 / 20)
+}
+
+
+
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let mut state = Universe{width:20, height:20,
+            cells: vec![Cell::Floor; 20 * 20],
+        };
+    
+        // Make the boundaries walls
+        for x in 0..20 {
+            state.cells[xy_idx(x, 0)] = Cell::Wall;
+            state.cells[xy_idx(x, 19)] = Cell::Wall;
+        }
+        for y in 0..20 {
+            state.cells[xy_idx(0, y)] = Cell::Wall;
+            state.cells[xy_idx(19, y)] = Cell::Wall;
+        }
+    
+        //Player
+        state.cells[xy_idx(1,1)] = Cell::Player;
+
+        // We'll return the state with the short-hand
+        state
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn get_cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+}
+
 pub fn main() {
- let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    log!("We have a context {:?}", context);
-
-    //clear
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str("black"));
-    context.fill_rect(0.0, 0.0, 800.0, 600.0);
+    let gs = Universe::new();
+    //debug
+    log!("We have a universe");
 }
 
 
