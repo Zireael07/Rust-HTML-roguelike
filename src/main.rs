87bc4d26f1af0ed6ec215f33af0e27c745a08392
Zireael07 <zi@mail.com>
2020-11-15 16:20:17 +0100

    new: proof of concept of noise map

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ use serde::{Serialize, Deserialize};
 use serde_json::json;
 
 //our stuff
+
+//3rd party vendored in
+mod fastnoise;
+use fastnoise::*;
+
 mod map;
 use map::*;
 mod fov;
@@ -196,6 +201,39 @@ impl Universe {
             ecs_world: World::new(),
         };
     
+        //mapgen
+
+        //noise
+        //generate noise
+        let mut rng = rand::thread_rng();
+        let mut noise = FastNoise::seeded(10001 as u64);
+        //let mut noise = FastNoise::seeded(rng.gen_range(1, 65537) as u64);
+        noise.set_noise_type(NoiseType::SimplexFractal);
+        noise.set_fractal_type(FractalType::FBM);
+        noise.set_fractal_octaves(1);
+        noise.set_fractal_gain(0.5);
+        noise.set_fractal_lacunarity(2.0);
+        //noise.set_frequency(0.085);
+        noise.set_frequency(0.45);
+
+        for x in 0..20 {
+            for y in 0..20 {
+                let mut n = noise.get_noise(x as f32, y as f32);
+                n = n*-255 as f32; //because defaults are vanishingly small
+                log!("{}", &format!("Noise: x{}y{} {}", x, y, n));
+                if n > 125.0 || n < -125.0 {
+                    state.tiles[xy_idx(x,y)] = Cell::Wall as u8;
+                    // opaque
+                    state.fov_data.set_transparent(x as usize, y as usize, false);
+                } else {
+                    //state.tiles[xy_idx(x,y)] = Cell:Floor as u8
+                }
+                //log!("{}", &format!("Tile: x{} y{} {}", x,y, state.tiles[xy_idx(x,y)]));
+            }
+        }
+      
+
+
         // Make the boundaries walls
         for x in 0..20 {
             state.tiles[xy_idx(x, 0)] = Cell::Wall as u8;
@@ -318,6 +356,9 @@ impl Universe {
                 }
             }
                  
+        }
+        else {
+            log!("{}", &format!("Blocked move to {}, {} ", new_position.0,new_position.1))
         }
     }
 
