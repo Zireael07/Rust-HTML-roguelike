fb4d43052c78f2b5a9edb1ab6448d0ec712ac67b
Zireael07 <katarzynamosz@gmail.com>
2020-12-05 17:46:48 +0000

    Deploying to gh-pages from  @ fc460694801c61b3d8b993485b0678dbf34798b8 🚀

--- rust_web_roguelike.js
+++ rust_web_roguelike.js
@@ -220,7 +220,7 @@ export const Renderable = Object.freeze({ Thug:0,Knife:1,Medkit:2,Barkeep:3, });
 export const Command = Object.freeze({ MoveLeft:0,MoveRight:1,MoveDown:2,MoveUp:3,GetItem:4,Inventory:5,SaveGame:6, });
 /**
 */
-export const Cell = Object.freeze({ Floor:0,Wall:1,Grass:2,Tree:3,FloorIndoor:4, });
+export const Cell = Object.freeze({ Floor:0,Wall:1,Grass:2,Tree:3,FloorIndoor:4,Door:5, });
 /**
 */
 export class Universe {
