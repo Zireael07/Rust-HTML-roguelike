cc0c473df316045f59dd644ec041a0c24b25053b
Zireael07 <zi@mail.com>
2021-03-13 15:08:24 +0100

    fix: dev build script uses wasm-pack to match CI

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # build
-cargo build --target wasm32-unknown-unknown --release
+#cargo build --target wasm32-unknown-unknown --release
 
 # build wasm to www
 # file name matches output file (cargo package name) above
-wasm-bindgen ./target/wasm32-unknown-unknown/release/rust_web_roguelike.wasm --out-dir docs --web --no-typescript
+#wasm-bindgen ./target/wasm32-unknown-unknown/release/rust_web_roguelike.wasm --out-dir docs --web --no-typescript
+
+wasm-pack build --out-name rust_web_roguelike --out-dir docs --target web --no-typescript
